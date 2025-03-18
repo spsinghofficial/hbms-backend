b2b47f5e180bb35270ac8ac8a8eaf9a45c570a53
Surinder Sidhu <surindersidhu16@gmail.com>
2025-03-17 20:29:30 -0400

    added swagger documentation

--- hotel-service/src/main/java/com/netneus/hotel_service/controllers/HotelController.java
+++ hotel-service/src/main/java/com/netneus/hotel_service/controllers/HotelController.java
@@ -2,6 +2,11 @@ package com.netneus.hotel_service.controllers;
 
 import com.netneus.hotel_service.model.Hotel;
 import com.netneus.hotel_service.services.HotelService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/hotel")
+@Tag(name = "Hotel Management", description = "Hotel Management API")
 public class HotelController {
     private final HotelService hotelService;
 
@@ -27,6 +33,11 @@ public class HotelController {
 
     // Get all hotels
     @GetMapping
+    @Operation(summary = "Get list of all hotels", description = "Fetch all hotels managed by given user",
+     responses = {
+            @ApiResponse(responseCode = "200", description = "Hotels fetched successfully"),
+            @ApiResponse(responseCode = "401", description = "User is unauthorized")
+        })
     public ResponseEntity<List<Hotel>> getAllHotels() {
         List<Hotel> hotels = hotelService.getAllHotels();
         return new ResponseEntity<>(hotels, HttpStatus.OK);
@@ -34,6 +45,7 @@ public class HotelController {
 
     // Get a hotel by ID
     @GetMapping("/{id}")
+    @Operation(summary = "Get hotel by ID", description = "Fetch a hotel using its ID")
     public ResponseEntity<Hotel> getHotelById(@PathVariable int id) {
         return hotelService.getHotelById(id)
                 .map(hotel -> new ResponseEntity<>(hotel, HttpStatus.OK))
--- hotel-service/src/main/java/com/netneus/hotel_service/model/Hotel.java
+++ hotel-service/src/main/java/com/netneus/hotel_service/model/Hotel.java
@@ -1,5 +1,6 @@
 package com.netneus.hotel_service.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -7,10 +8,19 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder 
 @EqualsAndHashCode(callSuper=true)
 public class Hotel extends BaseModel{
+    @Schema(description = "Hotel address", example = "123 Main St", required = true)
     private String address;
+
+    @Schema(description = "City where the hotel is located", example = "Toronto", required = true)
     private String city;
+
+    @Schema(description = "State or province", example = "Ontario")
     private String state;
+
+    @Schema(description = "Postal or ZIP code", example = "M5A 1A1", required = true)
     private String zip;
+
+    @Schema(description = "Indicates if the hotel is active", example = "true")
     private boolean isActive;
 
 }
